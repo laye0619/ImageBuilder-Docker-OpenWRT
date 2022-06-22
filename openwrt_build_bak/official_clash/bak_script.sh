37d97c0f19979d2f7d88bc18af28f91693a8bbbb
laye0619 <laye0619@gmail.com>
2022-06-22 19:39:06 +0800

    new

--- openwrt_build_bak/official_clash/bak_script.sh
+++ openwrt_build_bak/official_clash/bak_script.sh
@@ -7,10 +7,10 @@ config_folder=config
 cd /home/layewang/Docker/project/ImageBuilder-Docker-OpenWRT/openwrt_build_bak/official_clash/
 mkdir -p $files_folder/$etc_folder/$config_folder
 
-docker cp openwrt_official:/$etc_folder/$config_folder/ddns ./$files_folder/$etc_folder/$config_folder/ddns
-docker cp openwrt_official:/$etc_folder/$config_folder/dhcp ./$files_folder/$etc_folder/$config_folder/dhcp
-docker cp openwrt_official:/$etc_folder/$config_folder/firewall ./$files_folder/$etc_folder/$config_folder/firewall
-docker cp openwrt_official:/$etc_folder/$config_folder/network ./$files_folder/$etc_folder/$config_folder/network
+docker cp openwrt_clash:/$etc_folder/$config_folder/ddns ./$files_folder/$etc_folder/$config_folder/ddns
+docker cp openwrt_clash:/$etc_folder/$config_folder/dhcp ./$files_folder/$etc_folder/$config_folder/dhcp
+docker cp openwrt_clash:/$etc_folder/$config_folder/firewall ./$files_folder/$etc_folder/$config_folder/firewall
+docker cp openwrt_clash:/$etc_folder/$config_folder/network ./$files_folder/$etc_folder/$config_folder/network
 
-docker cp openwrt_official:/$etc_folder/firewall.user ./$files_folder/$etc_folder/firewall.user
-docker cp openwrt_official:/$etc_folder/rc.local ./$files_folder/$etc_folder/rc.local
+docker cp openwrt_clash:/$etc_folder/firewall.user ./$files_folder/$etc_folder/firewall.user
+docker cp openwrt_clash:/$etc_folder/rc.local ./$files_folder/$etc_folder/rc.local
