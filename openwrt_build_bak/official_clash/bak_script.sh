987e06fb463b28611cc12a0f92d013f175cfa418
laye0619 <laye0619@gmail.com>
2022-06-22 14:35:39 +0800

    add bak for official_clash

---
+++ openwrt_build_bak/official_clash/bak_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+files_folder=files_$(date "+%m%d")
+etc_folder=etc
+config_folder=config
+
+cd /home/layewang/Docker/project/ImageBuilder-Docker-OpenWRT/openwrt_build_bak/official_clash/
+mkdir -p $files_folder/$etc_folder/$config_folder
+
+docker cp openwrt_official:/$etc_folder/$config_folder/ddns ./$files_folder/$etc_folder/$config_folder/ddns
+docker cp openwrt_official:/$etc_folder/$config_folder/dhcp ./$files_folder/$etc_folder/$config_folder/dhcp
+docker cp openwrt_official:/$etc_folder/$config_folder/firewall ./$files_folder/$etc_folder/$config_folder/firewall
+docker cp openwrt_official:/$etc_folder/$config_folder/network ./$files_folder/$etc_folder/$config_folder/network
+
+docker cp openwrt_official:/$etc_folder/firewall.user ./$files_folder/$etc_folder/firewall.user
+docker cp openwrt_official:/$etc_folder/rc.local ./$files_folder/$etc_folder/rc.local
