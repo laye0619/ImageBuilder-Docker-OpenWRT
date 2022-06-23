a0df8b3b466ef898ad527878d0adaac6a5928955
laye0619 <laye0619@gmail.com>
2022-06-23 15:41:20 +0800

    add random port

--- clash_rule/get_clash_config.py
+++ clash_rule/get_clash_config.py
@@ -1,3 +1,4 @@
+import random
 import yaml
 import requests
 
@@ -10,6 +11,12 @@ config_content = yaml.safe_load(resp.text)
 with open('./clash_rule/my_config_need_to_add_into.yaml', 'r') as f:
     my_config = yaml.safe_load(f)
     f.close()
+
+# 随机生成0-9数字，填充端口号
+port_last_number = random.randint(1, 9)
+for item in my_config['proxies']:
+    item['port'] = int(float(item['port'])/10)*10+port_last_number
+
 config_content['proxies'] = my_config['proxies']
 config_content['proxy-groups'] = my_config['proxy-groups']
 config_content['rules'] = my_config['rules']+config_content['rules']
