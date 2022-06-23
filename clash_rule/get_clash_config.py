7d1354cf39415e1d4cd49d2bef16c012a9e4f05a
laye0619 <laye0619@gmail.com>
2022-06-23 10:39:35 +0800

    add auto-gen clash config

---
+++ clash_rule/get_clash_config.py
@@ -0,0 +1,18 @@
+import yaml
+import requests
+
+resp = requests.get(
+    'https://ghproxy.com/https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/main/LAZY_RULES/clash.yaml')
+config_content = yaml.safe_load(resp.text)
+
+with open('./clash_rule/my_config_need_to_add_into.yaml', 'r') as f:
+    my_config = yaml.safe_load(f)
+    f.close()
+config_content['proxies'] = my_config['proxies']
+config_content['proxy-groups'] = my_config['proxy-groups']
+config_content['rules'] = my_config['rules']+config_content['rules']
+config_content.pop('proxy-providers')
+
+with open('./clash_rule/my_clash_config.yaml', 'w') as f:
+    f.write(yaml.dump(config_content))
+    f.close()
