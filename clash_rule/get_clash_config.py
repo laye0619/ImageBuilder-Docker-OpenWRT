5d723f4149c9554b9e7d41410deec3707ef2cffe
laye0619 <laye0619@gmail.com>
2022-06-26 11:28:20 +0800

    new

--- clash_rule/get_clash_config.py
+++ clash_rule/get_clash_config.py
@@ -3,12 +3,15 @@ import random
 import yaml
 import requests
 
+RULE_PROVIDERS_FILENAME = 'loyalsoldier_providers_manual.yaml'
+RULE_FILENAME = 'loyalsoldier_rules_manual.yaml'
+
 
 def get_basic_part() -> dict:
-    resp = requests.get(
-        'https://ghproxy.com/https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/main/LAZY_RULES/Clash_Premium.yaml')
     # resp = requests.get(
-    #     'https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/main/LAZY_RULES/Clash_Premium.yaml')
+    #     'https://ghproxy.com/https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/main/LAZY_RULES/Clash_Premium.yaml')
+    resp = requests.get(
+        'https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/main/LAZY_RULES/Clash_Premium.yaml')
     config_content = yaml.safe_load(resp.text)
     config_content.pop('proxies')
     config_content.pop('proxy-groups')
@@ -22,7 +25,8 @@ def get_basic_part() -> dict:
 
     rules_dict = {}
     rules_dict['rules'] = config_content['rules']
-    rules_dict['rules'] = [x.replace(',Proxy',',PROXY') for x in rules_dict['rules']]
+    rules_dict['rules'] = [x.replace(',Proxy', ',PROXY')
+                           for x in rules_dict['rules']]
     with open('./clash_rule/hackl0us_rules.yaml', 'w') as f:
         f.write(yaml.dump(rules_dict, sort_keys=False))
         f.close()
@@ -43,7 +47,7 @@ def get_proxies_and_groups_part() -> dict:
     return my_config
 
 
-def get_rules_part(filename: str = 'hackl0us_rules.yaml') -> dict:
+def get_rules_part(filename: str) -> dict:
     with open('./clash_rule/'+filename, 'r') as f:
         my_config = yaml.safe_load(f)
         f.close()
@@ -54,7 +58,7 @@ def get_rules_part(filename: str = 'hackl0us_rules.yaml') -> dict:
     return my_config
 
 
-def get_rule_providers_part(filename: str = 'hackl0us_providers.yaml') -> dict:
+def get_rule_providers_part(filename: str) -> dict:
     with open('./clash_rule/'+filename, 'r') as f:
         my_config = yaml.safe_load(f)
         f.close()
@@ -63,9 +67,8 @@ def get_rule_providers_part(filename: str = 'hackl0us_providers.yaml') -> dict:
 
 my_clash_config = get_basic_part()
 my_clash_config.update(get_proxies_and_groups_part())
-my_clash_config.update(get_rule_providers_part(
-    'loyalsoldier_providers_manual.yaml'))
-my_clash_config.update(get_rules_part('loyalsoldier_rules_manual.yaml'))
+my_clash_config.update(get_rule_providers_part(RULE_PROVIDERS_FILENAME))
+my_clash_config.update(get_rules_part(RULE_FILENAME))
 with open('./clash_rule/my_clash_config.yaml', 'w') as f:
     f.write(yaml.dump(my_clash_config, sort_keys=False))
     f.close()
