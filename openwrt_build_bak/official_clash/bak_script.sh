8069c44b44bba596d8e408dbc111adb0315f5e70
laye0619 <laye0619@gmail.com>
2022-07-01 10:42:08 +0800

    change bak script format

--- openwrt_build_bak/official_clash/bak_script.sh
+++ openwrt_build_bak/official_clash/bak_script.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-files_folder=files_$(date "+%m%d")
-etc_folder=etc
-crontabs_folder=crontabs
-config_folder=config
-sysctld_folder=sysctl.d
 docker_container_name=openwrt_official
-
+files_folder=files_$(date "+%m%d")
 cd /home/layewang/Docker/project/ImageBuilder-Docker-OpenWRT/openwrt_build_bak/official_clash/
-mkdir -p $files_folder/$etc_folder/$config_folder
-mkdir $files_folder/$etc_folder/$crontabs_folder
-mkdir $files_folder/$etc_folder/$sysctld_folder
 
+etc_folder=etc
+mkdir -p $files_folder/$etc_folder
+docker cp $docker_container_name:/$etc_folder/firewall.user ./$files_folder/$etc_folder/firewall.user
+docker cp $docker_container_name:/$etc_folder/rc.local ./$files_folder/$etc_folder/rc.local
+
+crontabs_folder=crontabs
+mkdir $files_folder/$etc_folder/$crontabs_folder
+docker cp $docker_container_name:/$etc_folder/$crontabs_folder/root ./$files_folder/$etc_folder/$crontabs_folder/root
+
+config_folder=config
+mkdir $files_folder/$etc_folder/$config_folder
 docker cp $docker_container_name:/$etc_folder/$config_folder/ddns ./$files_folder/$etc_folder/$config_folder/ddns
 docker cp $docker_container_name:/$etc_folder/$config_folder/dhcp ./$files_folder/$etc_folder/$config_folder/dhcp
 docker cp $docker_container_name:/$etc_folder/$config_folder/firewall ./$files_folder/$etc_folder/$config_folder/firewall
 docker cp $docker_container_name:/$etc_folder/$config_folder/network ./$files_folder/$etc_folder/$config_folder/network
 docker cp $docker_container_name:/$etc_folder/$config_folder/openclash ./$files_folder/$etc_folder/$config_folder/openclash
 docker cp $docker_container_name:/$etc_folder/$config_folder/system ./$files_folder/$etc_folder/$config_folder/system
-
 sed -i "/option password/c \\\toption password \'INSERT_CLOUDFLARE_TOKEN\'" ./$files_folder/$etc_folder/$config_folder/ddns
 
-docker cp $docker_container_name:/$etc_folder/$crontabs_folder/root ./$files_folder/$etc_folder/$crontabs_folder/root
-
-docker cp $docker_container_name:/$etc_folder/firewall.user ./$files_folder/$etc_folder/firewall.user
-docker cp $docker_container_name:/$etc_folder/rc.local ./$files_folder/$etc_folder/rc.local
-
+sysctld_folder=sysctl.d
+mkdir $files_folder/$etc_folder/$sysctld_folder
 docker cp $docker_container_name:/$etc_folder/$sysctld_folder/sysctl-br-netfilter-ip.conf ./$files_folder/$etc_folder/$sysctld_folder/sysctl-br-netfilter-ip.conf
-# docker cp $docker_container_name:/$etc_folder/$sysctld_folder/net-ipv6-conf.conf ./$files_folder/$etc_folder/$sysctld_folder/net-ipv6-conf.conf
+
+
+
+# openclash_custom_folder=custom
+# mkdir $files_folder/$etc_folder/$openclash_custom_folder
+# docker cp $docker_container_name:/$etc_folder/$openclash_custom_folder/openclash_force_sniffing_domain.yaml ./$files_folder/$etc_folder/$openclash_custom_folder/openclash_force_sniffing_domain.yaml
+# docker cp $docker_container_name:/$etc_folder/$openclash_custom_folder/openclash_sniffing_domain_filter.yaml ./$files_folder/$etc_folder/$sysctld_folder/net-ipv6-conf.conf
+# docker cp $docker_container_name:/$etc_folder/$openclash_custom_folder/openclash_sniffing_port_filter.yaml ./$files_folder/$etc_folder/$sysctld_folder/net-ipv6-conf.conf
 
